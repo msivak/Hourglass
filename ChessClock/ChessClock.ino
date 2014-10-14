e31bcec690b81da05859393c4945c7dd4da17c8b
Mark Sivak <sivak.mark@gmail.com>
2014-10-13 22:54:18 -0400

    Arduino Features
    Added the ability to change players and the pause button to the arduino
    code. Issue with the time being ticked off needs to be fixed.

--- ChessClock/ChessClock.ino
+++ ChessClock/ChessClock.ino
@@ -2,15 +2,16 @@
 
 #include "Timer.h"
 
-
-
-
 boolean activePlayer;
 byte minute, second;
 int p1Time[2], p2Time[2];
 boolean playing = false;
 boolean flip = false;
 int gameTime;
+boolean pause = true;
+boolean pauseHold = true;
+String timeText = "";
+String timeText2 = "";
 
 Timer t;
 
@@ -27,15 +28,39 @@ void loop(){
   if(playing){
     t.update();
     playerSwitch();
+    pauseSwitch();
   }
 }
 
 void timer(){
-  p1Time[1]--;
-  if(p1Time[1] == 0){
-    p1Time[0]--;
-    p1Time[1] = 60;
-  }
+  if(activePlayer){
+     if(p1Time[1] <= 0){
+        timeText = String(p1Time[0])+":00";
+        p1Time[0]--;
+        p1Time[1] = 60;
+      }
+      else if(p1Time[1] < 10){
+        timeText = String(p1Time[0])+":0"+String(p1Time[1]);
+      }
+      else{
+        timeText = String(p1Time[0])+":"+String(p1Time[1]);
+      }
+      p1Time[1]--;
+    }
+    else if(activePlayer == false){
+           if(p2Time[1] <= 0){
+        timeText2 = String(p2Time[0])+":00";
+        p2Time[0]--;
+        p2Time[1] = 60;
+      }
+      else if(p2Time[1] < 10){
+        timeText2 = String(p2Time[0])+":0"+String(p2Time[1]);
+      }
+      else{
+        timeText2 = String(p2Time[0])+":"+String(p2Time[1]);
+      }
+      p2Time[1]--;
+    }
   serialWrite();
 }
 
@@ -48,11 +73,24 @@ void playerSwitch(){
  }
 }
 
+void pauseSwitch(){
+  if(digitalRead(12) == HIGH){
+    if(pauseHold){
+      pause = !pause;
+      pauseHold = false;
+    }
+  }
+  if(digitalRead(12) == LOW){
+    pauseHold = true;
+  }
+}
+
 void serialRead(){
   if(Serial.available() > 0){
-    p1Time[0] = Serial.parseInt();
-    Serial.print(p1Time[0]);
+    p1Time[0] = p2Time[0] = Serial.parseInt();
+    //Serial.print(p1Time[0]);
     p1Time[0]--;
+    p2Time[0]--;
     if(p1Time[0] != 0){
     playing = true;
     }
@@ -63,8 +101,15 @@ void serialRead(){
 void serialWrite(){
   //Serial.write(byte(p1Time[0]));
   //Serial.write(byte(p1Time[1]));
-  Serial.print(p1Time[0]);
-  Serial.print(":");
-  Serial.print(p1Time[1]);
+  Serial.print(activePlayer);
+  Serial.print(" ");
+  Serial.print(pause);
+  Serial.print(" ");
+  if(activePlayer){
+    Serial.print(timeText); 
+  }
+  else{
+    Serial.print(timeText2);
+  }
   Serial.println();
 }
